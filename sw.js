108ffe399a2a17db65c4428e48d4a99da9645565
dantifrance <dantifrance@gmail.com>
2026-08-23 01:07:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,2 @@
-const CACHE='board-games-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./config.js','./data.json','./manifest.json','./collection.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.registration.unregister())));
